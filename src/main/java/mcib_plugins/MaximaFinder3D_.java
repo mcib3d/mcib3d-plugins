c0756122065adcac4b6c26e13b3496267e88a3af
Thomas Boudier <thomas.boudier@upmc.fr>
2016-03-11 12:26:59 +0800

    Maximum finder, with noise parameter (v1)

--- src/main/java/mcib_plugins/MaximaFinder3D_.java
+++ src/main/java/mcib_plugins/MaximaFinder3D_.java
@@ -7,6 +7,7 @@ package mcib_plugins;
 
 import ij.IJ;
 import ij.ImagePlus;
+import ij.gui.GenericDialog;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 import mcib3d.image3d.ImageInt;
@@ -19,6 +20,9 @@ import mcib3d.image3d.processing.MaximaFinder;
 public class MaximaFinder3D_ implements PlugInFilter {
 
     ImagePlus plus;
+    float noise = 100;
+    float rxy = 1.5f;
+    float rz = 1.5f;
 
     @Override
     public int setup(String arg, ImagePlus imp) {
@@ -29,16 +33,24 @@ public class MaximaFinder3D_ implements PlugInFilter {
 
     @Override
     public void run(ImageProcessor ip) {
-        ImageInt img = ImageInt.wrap(plus);
-        int noise=(int) IJ.getNumber("Noise ", 100);
-        MaximaFinder test = new MaximaFinder(img, noise);
-        IJ.log("compute list");
-        test.computeMaximaList();
-        IJ.log("compute peaks");
-        test.drawPeaks();
-         IJ.log("show peaks");
-        test.getPeaks().show();
-
+        if (dialog()) {
+            ImageInt img = ImageInt.wrap(plus);
+            MaximaFinder test = new MaximaFinder(img, noise);
+            test.getPeaks().show();
+        }
     }
 
+    private boolean dialog() {
+        GenericDialog dia = new GenericDialog("Maxima Finder");
+        dia.addNumericField("RadiusXY", rxy, 2, 5, "pixel");
+        dia.addNumericField("RadiusZ", rz, 2, 5, "pixel");
+        dia.addNumericField("Noise", noise, 2);
+        dia.showDialog();
+        rxy = (float) dia.getNextNumber();
+        rz = (float) dia.getNextNumber();
+        noise = (float) dia.getNextNumber();
+
+        return dia.wasOKed();
+
+    }
 }
