98ad5785b793c1486ee6f8f7948307edac056619
Thomas Boudier <thomas.boudier@upmc.fr>
2016-04-06 17:05:45 +0800

    Remove local function normalize

--- src/main/java/mcib_plugins/EDT_3D.java
+++ src/main/java/mcib_plugins/EDT_3D.java
@@ -19,10 +19,10 @@ import mcib3d.image3d.distanceMap3d.EDT;
  *
  */
 public class EDT_3D implements PlugIn {
-    
+
     boolean inverse = false;
     int threshold = 1;
-    
+
     @Override
     public void run(String arg) {
 //        ImagePlus imp = IJ.getImage();
@@ -35,16 +35,16 @@ public class EDT_3D implements PlugIn {
         }
         String[] namesStructure = new String[nbima];
         String[] namesMask = new String[nbima + 1];
-        
+
         namesMask[0] = "None";
         for (int i = 0; i < nbima; i++) {
             namesStructure[i] = WindowManager.getImage(i + 1).getShortTitle();
             namesMask[i + 1] = WindowManager.getImage(i + 1).getShortTitle();
         }
-        
+
         int struct = 0;
         int mask = nbima > 1 ? nbima - 1 : 0;
-        
+
         GenericDialog dia = new GenericDialog("EDT");
         dia.addChoice("Map", new String[]{"EDT", "EVF", "Both"}, "EDT");
         dia.addChoice("Image", namesStructure, namesStructure[struct]);
@@ -57,7 +57,7 @@ public class EDT_3D implements PlugIn {
         mask = dia.getNextChoiceIndex();
         threshold = (int) dia.getNextNumber();
         inverse = dia.getNextBoolean();
-        
+
         if (dia.wasOKed()) {
             try {
                 ImageHandler img = ImageHandler.wrap(WindowManager.getImage(struct + 1));
@@ -76,7 +76,7 @@ public class EDT_3D implements PlugIn {
                             imgMask = (ImageInt) ImageHandler.wrap(WindowManager.getImage(mask));
                         }
                         IJ.log("Normalizing Distance Map (EVF) ...");
-                        normalizeDistanceMap(r2, imgMask);
+                        EDT.normalizeDistanceMap(r2, imgMask, true);
                         if (mask > 0) {
                             r2.intersectMask(imgMask);
                         }
@@ -88,63 +88,63 @@ public class EDT_3D implements PlugIn {
             }
         }
     }
-    
-    public void normalizeDistanceMap(ImageFloat distanceMap, ImageInt mask) {
-        int count = 0;
-        Vox[] idx = new Vox[mask.countMaskVolume()];
-        double volume = idx.length;
-        for (int z = 0; z < distanceMap.sizeZ; z++) {
-            for (int xy = 0; xy < distanceMap.sizeXY; xy++) {
-                if (mask.getPixelInt(xy, z) != 0) {
-                    idx[count] = new Vox(distanceMap.pixels[z][xy], xy, z);
-                    count++;
-                }
-            }
-        }
-        Arrays.sort(idx);
-        for (int i = 0; i < idx.length - 1; i++) {
-            // gestion des repetitions
-            if (idx[i + 1].distance == idx[i].distance) {
-                int j = i + 1;
-                while (j < (idx.length - 1) && idx[i].distance == idx[j].distance) {
-                    j++;
-                }
-                double median = (i + j) / 2d;
-                for (int k = i; k <= j; k++) {
-                    idx[k].index = median;
-                }
-                i = j;
-            } else {
-                idx[i].index = i;
-            }
-        }
-        if (idx[idx.length - 1].index == 0) {
-            idx[idx.length - 1].index = idx.length - 1;
-        }
-        for (int i = 0; i < idx.length; i++) {
-            distanceMap.pixels[idx[i].z][idx[i].xy] = (float) (idx[i].index / volume);
-        }
-    }
-    
+
+//    public void normalizeDistanceMap(ImageFloat distanceMap, ImageInt mask) {
+//        int count = 0;
+//        Vox[] idx = new Vox[mask.countMaskVolume()];
+//        double volume = idx.length;
+//        for (int z = 0; z < distanceMap.sizeZ; z++) {
+//            for (int xy = 0; xy < distanceMap.sizeXY; xy++) {
+//                if (mask.getPixelInt(xy, z) != 0) {
+//                    idx[count] = new Vox(distanceMap.pixels[z][xy], xy, z);
+//                    count++;
+//                }
+//            }
+//        }
+//        Arrays.sort(idx);
+//        for (int i = 0; i < idx.length - 1; i++) {
+//            // gestion des repetitions
+//            if (idx[i + 1].distance == idx[i].distance) {
+//                int j = i + 1;
+//                while (j < (idx.length - 1) && idx[i].distance == idx[j].distance) {
+//                    j++;
+//                }
+//                double median = (i + j) / 2d;
+//                for (int k = i; k <= j; k++) {
+//                    idx[k].index = median;
+//                }
+//                i = j;
+//            } else {
+//                idx[i].index = i;
+//            }
+//        }
+//        if (idx[idx.length - 1].index == 0) {
+//            idx[idx.length - 1].index = idx.length - 1;
+//        }
+//        for (Vox idx1 : idx) {
+//            distanceMap.pixels[idx1.z][idx1.xy] = (float) (idx1.index / volume);
+//        }
+//    }
+
     protected class Vox implements Comparable<Vox> {
-        
+
         float distance;
         double index;
         int xy, z;
-        
+
         public Vox(float distance, int xy, int z) {
             this.distance = distance;
             this.xy = xy;
             this.z = z;
         }
-        
+
         public Vox(float distance, double index, int xy, int z) {
             this.distance = distance;
             this.index = index;
             this.xy = xy;
             this.z = z;
         }
-        
+
         @Override
         public int compareTo(Vox v) {
             if (distance > v.distance) {
