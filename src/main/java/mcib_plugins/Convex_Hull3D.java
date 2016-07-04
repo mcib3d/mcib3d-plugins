7cac25aa3bd8e93bfa871cb9172eeb9f4c03ee4f
mcib3d <thomas.boudier@upmc.fr>
2016-07-04 16:50:37 +0800

    deal with image calibration

--- src/main/java/mcib_plugins/Convex_Hull3D.java
+++ src/main/java/mcib_plugins/Convex_Hull3D.java
@@ -5,16 +5,16 @@ import ij.ImagePlus;
 import ij.measure.Calibration;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
-import java.text.NumberFormat;
 import mcib3d.geom.Object3D;
-import mcib3d.geom.Object3DSurface;
 import mcib3d.geom.Object3DVoxels;
 import mcib3d.geom.ObjectCreator3D;
-import mcib3d.geom.Vector3D;
-import mcib3d.image3d.legacy.IntImage3D;
+import mcib3d.image3d.ImageHandler;
+import mcib3d.image3d.ImageInt;
+
+import java.text.NumberFormat;
 
 /**
- * plugin to fit an 3D ellipsoid to a shape
+ * plugin to compute the 3D hull, based on quickHull
  *
  * @author Thomas BOUDIER
  * @created avril 2003
@@ -25,9 +25,9 @@ public class Convex_Hull3D implements PlugInFilter {
     float rad;
 
     /**
-     * Main processing method for the Axes3D_ object
+     * Main processing method for the plugin
      *
-     * @param ip Description of the Parameter
+     * @param ip image to process, must be labelled
      */
     @Override
     public void run(ImageProcessor ip) {
@@ -42,48 +42,37 @@ public class Convex_Hull3D implements PlugInFilter {
                 unit = cal.getUnits();
             }
         }
+        // no need calibration for computing hull
+        ImageInt ima = ImageInt.wrap(imp);
+        ima.setScale(1, 1, "pix");
         //drawing of hulls
-        ObjectCreator3D hulls = new ObjectCreator3D(imp.getWidth(), imp.getHeight(), imp.getStackSize());
+        ObjectCreator3D hulls = new ObjectCreator3D(ima.sizeX, ima.sizeY, ima.sizeZ);
         hulls.setResolution(resXY, resZ, unit);
         Object3DVoxels obj;
         // all objects from count masks
-        IntImage3D ima = new IntImage3D(imp.getStack());
         int valmin = (int) ima.getMinAboveValue(0);
-        int valmax = (int) ima.getMaximum();
+        int valmax = (int) ima.getMax();
         for (int val = valmin; val <= valmax; val++) {
             IJ.showStatus("Trying obj " + val);
-            obj = new Object3DVoxels(imp, val);
-
+            obj = new Object3DVoxels(ima, val);
             NumberFormat nf = NumberFormat.getInstance();
             nf.setMaximumFractionDigits(3);
-
             if (obj.getVolumePixels() > 0) {
                 IJ.log("\nProcessing obj " + val);
-//                Object3DSurface surf = new Object3DSurface(obj.computeMeshSurface(false));
-//                surf = surf.getConvexObject();
-//                surf.multiThread = true;
-//                obj = new Object3DVoxels(surf.getVoxels());
-                Object3D objC = obj.getConvexObject(true);
+                Object3D objC = obj.getConvexObject();
                 objC.draw(hulls, val);
-                // obj.draw(hulls, val);
             }
         }
-        ImagePlus plus = new ImagePlus("Convex_Hulls", hulls.getStack());
+        ImageHandler imageHandler = hulls.getImageHandler();
         if (cal != null) {
-            plus.setCalibration(cal);
+            imageHandler.setCalibration(cal);
         }
-        plus.setSlice(plus.getStackSize()/2);
+        ImagePlus plus = imageHandler.getImagePlus();
+        plus.setSlice(plus.getStackSize() / 2);
         plus.setDisplayRange(0, valmax);
         plus.show();
     }
 
-    /**
-     * Description of the Method
-     *
-     * @param arg Description of the Parameter
-     * @param imp Description of the Parameter
-     * @return Description of the Return Value
-     */
     @Override
     public int setup(String arg, ImagePlus imp) {
         this.imp = imp;
