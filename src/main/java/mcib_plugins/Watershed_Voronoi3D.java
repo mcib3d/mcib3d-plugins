f3479ec888dd4096ba80faf95c62afcdb78404ab
mcib3d <thomas.boudier@upmc.fr>
2016-08-17 16:10:17 +0800

    New class for Voronoi Use  radius to contraints extends of voronoi zones

--- src/main/java/mcib_plugins/Watershed_Voronoi3D.java
+++ src/main/java/mcib_plugins/Watershed_Voronoi3D.java
@@ -6,16 +6,11 @@ package mcib_plugins;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.WindowManager;
+import ij.gui.GenericDialog;
 import ij.measure.Calibration;
 import ij.plugin.PlugIn;
-import mcib3d.geom.Object3DVoxels;
-import mcib3d.geom.ObjectCreator3D;
-import mcib3d.geom.Objects3DPopulation;
-import mcib3d.image3d.ImageFloat;
-import mcib3d.image3d.ImageHandler;
 import mcib3d.image3d.ImageInt;
-import mcib3d.image3d.distanceMap3d.EDT;
-import mcib3d.image3d.regionGrowing.Watershed3D;
+import mcib3d.image3d.regionGrowing.Watershed3DVoronoi;
 
 /**
  * Description of the Class
@@ -25,10 +20,10 @@ import mcib3d.image3d.regionGrowing.Watershed3D;
  */
 public class Watershed_Voronoi3D implements PlugIn {
     // seeds
-
     ImagePlus seedPlus = null;
-
-    boolean anim = false;
+    float radMax = 0;
+    boolean showLines = false;
+    boolean showEDT = false;
 
     @Override
     public void run(String arg) {
@@ -39,46 +34,33 @@ public class Watershed_Voronoi3D implements PlugIn {
             return;
         }
         seedPlus = WindowManager.getCurrentImage();
-        WatershedVoronoi();
+        if (Dialog())
+            WatershedVoronoi();
     }
 
     private void WatershedVoronoi() {
         IJ.log("");
         long t = System.currentTimeMillis();
-        float resXY = 1;
-        float resZ = 1;
-        Calibration cal = seedPlus.getCalibration();
-        if (cal != null) {
-            resXY = (float) cal.pixelWidth;
-            resZ = (float) cal.pixelDepth;
-        }
-        IJ.log("Computing EDT");
-        ImageInt imgSeeds = ImageInt.wrap(seedPlus);
-        ImageFloat edt = EDT.run(imgSeeds, 0, resXY, resZ, true, 0);
-        ImageHandler edt16 = edt.convertToShort(true);
-        edt16.invert();
-        edt16.show("EDT");
-        IJ.log("Computing watershed");
-        Watershed3D water = new Watershed3D(edt16, imgSeeds, 0, 0);
-        water.setAnim(anim);
-        ImageInt voronoi = water.getWatershedImage3D();
-        voronoi.show("VoronoiZones");
-        // lines
-        Object3DVoxels zero = new Object3DVoxels(voronoi, 0);
-        Objects3DPopulation pop = new Objects3DPopulation(voronoi);
-        ObjectCreator3D draw = new ObjectCreator3D(seedPlus.getWidth(), seedPlus.getHeight(), seedPlus.getNSlices());
-        draw.getImageHandler().fill(1);
-        zero.draw(draw, 0);
-        for (int o = 0; o < pop.getNbObjects(); o++) {
-            Object3DVoxels obj = (Object3DVoxels) pop.getObject(o);
-            obj.computeContours();
-            if (seedPlus.getNSlices() > 1)
-                obj.drawContours(draw, 0);
-            else {
-                obj.drawContoursXY(draw, 0, 0);
-            }
-        }
-        draw.getImageHandler().show("VoronoiLines");
+        Watershed3DVoronoi watershed3DVoronoi = new Watershed3DVoronoi(ImageInt.wrap(seedPlus), radMax);
+        watershed3DVoronoi.getVoronoiZones(showEDT).show("VoronoiZones");
+        if (showLines) watershed3DVoronoi.getVoronoiLines(true).show("VoronoiLines");
         IJ.log("Finished in " + (System.currentTimeMillis() - t) + " ms.");
+    }
+
+    private boolean Dialog() {
+        Calibration calibration = seedPlus.getCalibration();
+        String unit = "pixel";
+        if (calibration != null) unit = calibration.getUnits();
+        GenericDialog genericDialog = new GenericDialog("Voronoi");
+        genericDialog.addNumericField("Radius_Max (0 for no max)", radMax, 2, 10, unit);
+        genericDialog.addCheckbox("Show_EDT", showEDT);
+        genericDialog.addCheckbox("Show_Lines", showLines);
+        genericDialog.showDialog();
+        radMax = (float) genericDialog.getNextNumber();
+        if (radMax == 0) radMax = Float.MAX_VALUE;
+        showEDT = genericDialog.getNextBoolean();
+        showLines = genericDialog.getNextBoolean();
+
+        return genericDialog.wasOKed();
     }
 }
