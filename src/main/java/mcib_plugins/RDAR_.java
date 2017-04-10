e75855b52519f03eb280014ce818789ad7771716
mcib3d <thomas.boudier@upmc.fr>
2017-04-10 16:13:51 +0800

    Update RDAR, only ellipsoid fitting

--- src/main/java/mcib_plugins/RDAR_.java
+++ src/main/java/mcib_plugins/RDAR_.java
@@ -20,11 +20,7 @@ import java.util.ArrayList;
  */
 public class RDAR_ implements PlugInFilter {
     private ImagePlus imagePlus;
-    private int radiusX = 10;
-    private int radiusY = 10;
-    private int radiusZ = 10;
     private int minVolume = 100;
-    private boolean autoRadius = true;
 
     @Override
     public int setup(String arg, ImagePlus imp) {
@@ -39,11 +35,11 @@ public class RDAR_ implements PlugInFilter {
             Objects3DPopulation objects3DPopulation = new Objects3DPopulation(imageInt);
             // drawing
             ImageHandler drawIn = new ImageShort("Parts inside", imagePlus.getWidth(), imagePlus.getHeight(), imagePlus.getImageStackSize());
-            drawIn.setCalibration(imageInt.getCalibration());
+            drawIn.setScale(imageInt);
             ImageHandler drawOut = new ImageShort("Parts outside", imagePlus.getWidth(), imagePlus.getHeight(), imagePlus.getImageStackSize());
-            drawOut.setCalibration(imageInt.getCalibration());
+            drawOut.setScale(imageInt);
             ImageHandler drawEll = new ImageShort("Ellipsoid", imagePlus.getWidth(), imagePlus.getHeight(), imagePlus.getImageStackSize());
-            drawEll.setCalibration(imageInt.getCalibration());
+            drawEll.setScale(imageInt);
             for (Object3D object3D : objects3DPopulation.getObjectsList()) {
                 processObject(object3D, drawIn, drawOut, drawEll);
             }
@@ -58,33 +54,19 @@ public class RDAR_ implements PlugInFilter {
         double resZ = object3D.getResZ();
         IJ.log("Analysing object " + object3D.getValue());
         Object3DVoxels object3DVoxels = object3D.getObject3DVoxels();
-        if (autoRadius) {
-            double rad1 = object3DVoxels.getRadiusMoments(2);
-            double rad2 = Double.NaN;
-            if (!Double.isNaN(object3DVoxels.getMainElongation())) {
-                rad2 = rad1 / object3DVoxels.getMainElongation();
-            }
-            double rad3 = Double.NaN;
-            if (!Double.isNaN(object3DVoxels.getMedianElongation())) {
-                rad3 = rad2 / object3DVoxels.getMedianElongation();
-            }
-            radiusX = (int) Math.round(rad1 / resXY);
-            radiusY = (int) Math.round(rad2 / resXY);
-            radiusZ = (int) Math.round(rad3 / resZ);
 
-            IJ.log("Ellipsoid radii(pix)=" + radiusX + "," + radiusY + "," + radiusZ);
-        }
-
-        RDAR rdar = new RDAR(object3DVoxels, radiusX, radiusY, radiusZ);
+        RDAR rdar = new RDAR(object3D.getObject3DVoxels());
         IJ.log("Nb parts in : " + rdar.getPartsInNumber(minVolume) + ", nb parts out : " + rdar.getPartsOutNumber(minVolume));
         // volume
         ArrayList<Object3DVoxels> list = rdar.getPartsIn(minVolume);
         int vol = 0;
-        for (Object3DVoxels object3DVoxels1 : list) vol += object3DVoxels.getVolumePixels();
+        if (list != null)
+            for (Object3DVoxels object3DVoxels1 : list) vol += object3DVoxels.getVolumePixels();
         IJ.log("Volume In : " + vol);
         list = rdar.getPartsOut(minVolume);
         vol = 0;
-        for (Object3DVoxels object3DVoxels1 : list) vol += object3DVoxels.getVolumePixels();
+        if (list != null)
+            for (Object3DVoxels object3DVoxels1 : list) vol += object3DVoxels.getVolumePixels();
         IJ.log("Volume Out : " + vol);
 
         int color = object3DVoxels.getValue();
@@ -102,16 +84,8 @@ public class RDAR_ implements PlugInFilter {
 
     private boolean dialog() {
         GenericDialog genericDialog = new GenericDialog("RDAR");
-        genericDialog.addNumericField("Radius_X", radiusX, 1, 10, "pix");
-        genericDialog.addNumericField("Radius_Y", radiusY, 1, 10, "pix");
-        genericDialog.addNumericField("Radius_Z", radiusZ, 1, 10, "pix");
-        genericDialog.addCheckbox("Fit ellipsoid", autoRadius);
         genericDialog.addNumericField("Min volume parts", minVolume, 1, 10, "pix");
         genericDialog.showDialog();
-        autoRadius = genericDialog.getNextBoolean();
-        radiusX = (int) genericDialog.getNextNumber();
-        radiusY = (int) genericDialog.getNextNumber();
-        radiusZ = (int) genericDialog.getNextNumber();
         minVolume = (int) genericDialog.getNextNumber();
 
         return genericDialog.wasOKed();
