42ea52bae89a36b26dd37d2b6c9912eb79e5e60e
tboudier <thomas.boudier@upmc.fr>
2018-10-22 15:11:10 +1100

    improved hysteresis, faster

--- src/main/java/mcib_plugins/Hysteresis_Thresholding.java
+++ src/main/java/mcib_plugins/Hysteresis_Thresholding.java
@@ -28,32 +28,43 @@ public class Hysteresis_Thresholding implements PlugInFilter {
         GenericDialog gd = new GenericDialog("Hysteresis Thresholding");
         gd.addNumericField("High Threshold:", 128, 1);
         gd.addNumericField("Low Threshold:", 50, 1);
+        gd.addCheckbox("Show multi-threshold", false);
         gd.showDialog();
         if (gd.wasOKed()) {
             double high = gd.getNextNumber();
             double low = gd.getNextNumber();
-            ImagePlus hyst = hysteresis(plus, low, high);
+
+            ImagePlus hyst = hysteresis(plus, low, high, gd.getNextBoolean());
             hyst.setDisplayRange(0, 255);
             hyst.show();
         }
     }
 
-    public static ImagePlus hysteresis(ImagePlus image, double lowval, double highval) {
+
+    public static ImagePlus hysteresis(ImagePlus image, double lowval, double highval, boolean show) {
+        int HIGH = 255;
+        int LOW = 128;
         // first threshold the image
         ImageInt img = ImageInt.wrap(image);
-        ImageByte temp = new ImageByte("bin", img.sizeX, img.sizeY, img.sizeZ);
+        ImageByte temp = new ImageByte(image.getTitle() + "_Multi", img.sizeX, img.sizeY, img.sizeZ);
         for (int z = 0; z < img.sizeZ; z++) {
             for (int xy = 0; xy < img.sizeXY; xy++) {
                 if (img.getPixel(xy, z) > highval) {
-                    temp.setPixel(xy, z, 255);
+                    temp.setPixel(xy, z, HIGH);
                 } else if (img.getPixel(xy, z) > lowval) {
-                    temp.setPixel(xy, z, 128);
+                    temp.setPixel(xy, z, LOW);
                 }
             }
         }
+        ImageInt hyst;
+        if (show) {
+            temp.show();
+            hyst = temp.duplicate();
+        } else hyst = temp;
+        hyst.setTitle(image.getTitle() + "_Hyst");
         // connect 255 to 128
-        Flood3D.connect3D(temp, 128, 255);
+        Flood3D.connect3D(hyst, LOW, HIGH);
 
-        return temp.getImagePlus();
+        return hyst.getImagePlus();
     }
 }
