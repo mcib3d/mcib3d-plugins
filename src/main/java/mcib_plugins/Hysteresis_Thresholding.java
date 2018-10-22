161275910af91bb7a03c9d44b9a01a9ff416bf27
tboudier <thomas.boudier@upmc.fr>
2018-10-22 14:46:17 +1100

    new implementation of hysteresis thresholding using faster flooding

--- src/main/java/mcib_plugins/Hysteresis_Thresholding.java
+++ src/main/java/mcib_plugins/Hysteresis_Thresholding.java
@@ -9,12 +9,12 @@ import ij.ImagePlus;
 import ij.gui.GenericDialog;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
-import imagescience.image.Image;
-import mcib_plugins.processing.Thresholder;
+import mcib3d.image3d.ImageByte;
+import mcib3d.image3d.ImageInt;
+import mcib3d.image3d.processing.Flood3D;
 
 public class Hysteresis_Thresholding implements PlugInFilter {
-
-    ImagePlus plus = null;
+    ImagePlus plus;
 
     @Override
     public int setup(String arg, ImagePlus imp) {
@@ -25,30 +25,35 @@ public class Hysteresis_Thresholding implements PlugInFilter {
 
     @Override
     public void run(ImageProcessor ip) {
-        GenericDialog gd = new GenericDialog("Hysteresis Thresholding (ImageScience)");
+        GenericDialog gd = new GenericDialog("Hysteresis Thresholding");
         gd.addNumericField("High Threshold:", 128, 1);
         gd.addNumericField("Low Threshold:", 50, 1);
-        gd.addCheckbox("Connectivity 4", false);
         gd.showDialog();
         if (gd.wasOKed()) {
             double high = gd.getNextNumber();
             double low = gd.getNextNumber();
-            hysteresis(plus , low, high, gd.getNextBoolean());
-            plus.setDisplayRange(0, 255);
-            plus.updateAndRepaintWindow();
+            ImagePlus hyst = hysteresis(plus, low, high);
+            hyst.setDisplayRange(0, 255);
+            hyst.show();
         }
     }
-    
-    public static void hysteresis(ImagePlus image, double lowval, double highval, boolean lowConnectivity) {
-        final Image imp = Image.wrap(image);
-        final Thresholder thres = new Thresholder();
-        if (lowConnectivity) {
-            thres.hysteresisLowConnectivity(imp, lowval, highval);
-        } else {
-            thres.hysteresis(imp, lowval, highval);
-        }        
-//        final ImageInt img=ImageInt.wrap(image);
-//        ImageHandler.convertToByte(img);
-//        image=img.getImagePlus();
+
+    public static ImagePlus hysteresis(ImagePlus image, double lowval, double highval) {
+        // first threshold the image
+        ImageInt img = ImageInt.wrap(image);
+        ImageByte temp = new ImageByte("bin", img.sizeX, img.sizeY, img.sizeZ);
+        for (int z = 0; z < img.sizeZ; z++) {
+            for (int xy = 0; xy < img.sizeXY; xy++) {
+                if (img.getPixel(xy, z) > highval) {
+                    temp.setPixel(xy, z, 255);
+                } else if (img.getPixel(xy, z) > lowval) {
+                    temp.setPixel(xy, z, 128);
+                }
+            }
+        }
+        // connect 255 to 128
+        Flood3D.connect3D(temp, 128, 255);
+
+        return temp.getImagePlus();
     }
 }
