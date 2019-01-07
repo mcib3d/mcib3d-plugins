62c32b6f3f8e22ee9055ef33768fb40b9dffcea7
mcib3d <thomas.boudier@upmc.fr>
2019-01-07 15:31:55 +1100

    new faster hysteresis thresholding

--- src/main/java/mcib_plugins/Hysteresis_Thresholding.java
+++ src/main/java/mcib_plugins/Hysteresis_Thresholding.java
@@ -10,12 +10,18 @@ import ij.ImagePlus;
 import ij.gui.GenericDialog;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
+import mcib3d.geom.Object3DVoxels;
+import mcib3d.geom.Voxel3D;
 import mcib3d.image3d.ImageByte;
+import mcib3d.image3d.ImageHandler;
 import mcib3d.image3d.ImageInt;
+import mcib3d.image3d.ImageLabeller;
 import mcib3d.image3d.processing.Flood3D;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Hysteresis_Thresholding implements PlugInFilter {
     ImagePlus plus;
@@ -38,40 +44,54 @@ public class Hysteresis_Thresholding implements PlugInFilter {
             double high = gd.getNextNumber();
             double low = gd.getNextNumber();
 
+            Instant t0 = Instant.now();
             ImagePlus hyst = hysteresis(plus, low, high, gd.getNextBoolean());
+            Instant t1 = Instant.now();
             hyst.setDisplayRange(0, 255);
             hyst.show();
+            IJ.log("Hysteresis took " + Duration.between(t0, t1));
         }
     }
 
 
-    public static ImagePlus hysteresis(ImagePlus image, double lowval, double highval, boolean show) {
+    public ImagePlus hysteresis(ImagePlus image, double lowval, double highval, boolean show) {
         int HIGH = 255;
         int LOW = 128;
         // first threshold the image
         ImageInt img = ImageInt.wrap(image);
-        ImageByte temp = new ImageByte(image.getTitle() + "_Multi", img.sizeX, img.sizeY, img.sizeZ);
+        ImageByte multi = new ImageByte(image.getTitle() + "_Multi", img.sizeX, img.sizeY, img.sizeZ);
         for (int z = 0; z < img.sizeZ; z++) {
             for (int xy = 0; xy < img.sizeXY; xy++) {
                 if (img.getPixel(xy, z) > highval) {
-                    temp.setPixel(xy, z, HIGH);
+                    multi.setPixel(xy, z, HIGH);
                 } else if (img.getPixel(xy, z) > lowval) {
-                    temp.setPixel(xy, z, LOW);
+                    multi.setPixel(xy, z, LOW);
                 }
             }
         }
-        ImageInt hyst;
-        if (show) {
-            temp.show();
-            hyst = temp.duplicate();
-        } else hyst = temp;
-        hyst.setTitle(image.getTitle() + "_Hyst");
-        // connect 255 to 128
-        Instant t0 = Instant.now();
-        Flood3D.connect3D(hyst, LOW, HIGH);
-        Instant t1 = Instant.now();
-        IJ.log("Flooding took " + Duration.between(t0, t1));
+        if (show) multi.show();
+
+        ImageHandler thresholded = multi.thresholdAboveInclusive(LOW);
+        ImageLabeller labeller = new ImageLabeller();
+        ArrayList<Object3DVoxels> objects = labeller.getObjects(thresholded);
+
+        ImageHandler hyst = new ImageByte("Hyst_"+image.getTitle(), multi.sizeX, multi.sizeY, multi.sizeZ);
+        for (Object3DVoxels object3DVoxels : objects) {
+            if (hasOneVoxelValueRange(object3DVoxels, multi, HIGH, HIGH)) {
+                object3DVoxels.draw(hyst, 255);
+            }
+        }
 
         return hyst.getImagePlus();
+    }
+
+    private boolean hasOneVoxelValueRange(Object3DVoxels object3DVoxels, ImageHandler handler, int t0, int t1) {
+        LinkedList<Voxel3D> list = object3DVoxels.getVoxels();
+        for (Voxel3D vox : list) {
+            float pix = handler.getPixel(vox);
+            if ((pix >= t0) && (pix <= t1)) return true;
+        }
+
+        return false;
     }
 }
